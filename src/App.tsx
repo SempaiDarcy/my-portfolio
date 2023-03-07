83875affddeab1e1f155444078d8d391e9d0e75a
SempaiDarcy <mr_darcy_1@mail.ru>
2023-03-07 11:08:00 +0500

    Topbar finished

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {Contact} from "./components/contact/Contact";
 function App() {
     return (
         <div className={s.App}>
-            <Topbar/>
+                <Topbar/>
             <div className={s.sections}>
                 <Intro/>
                 <Portfolio/>
--- src/components/ContactButtonGroup/ContactButtonGroup.tsx
+++ src/components/ContactButtonGroup/ContactButtonGroup.tsx
@@ -5,16 +5,16 @@ import {AiOutlineMail} from "@react-icons/all-files/ai/AiOutlineMail";
 import {AiFillGithub} from "@react-icons/all-files/ai/AiFillGithub";
 import {FaTelegramPlane} from "@react-icons/all-files/fa/FaTelegramPlane";
 import {SiCodewars} from "@react-icons/all-files/si/SiCodewars";
-
+import s from './ContactButtonGroup.module.scss'
 
 export const ContactButtonGroup = () => {
     return (
-        <div>
-                <AiFillLinkedin/>
-                <AiOutlineMail/>
-                <AiFillGithub/>
-                <FaTelegramPlane/>
-                <SiCodewars/>
+        <div className={s.buttonGroup}>
+                <AiFillLinkedin className={s.icon}/>
+                <AiOutlineMail className={s.icon}/>
+                <AiFillGithub className={s.icon}/>
+                <FaTelegramPlane className={s.icon}/>
+                <SiCodewars className={s.icon}/>
         </div>
     )
 }
